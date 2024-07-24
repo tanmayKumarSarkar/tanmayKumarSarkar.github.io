b1c55fb6865aefdd74976e33d2dffa6949ae932d
tanmayKumarSarkar <tanmay.kr.sarkar@live.com>
2024-07-24 10:21:24 +0000

    commit jul 24 3;24

---
+++ src/components/Intro2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { parallaxImages } from "../constants";
+import NightSky from "./NightSky";
+
+function Intro() {
+
+  return (
+    <div className="intro-cointainer w-full h-full py-0 relative overflow-hidden">
+        
+    </div>
+  );
+}
+
+export default Intro;

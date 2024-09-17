1589d6fc34b0287395256278ad5d1574cecddf4e
tanmayKumarSarkar <tanmay.kr.sarkar@live.com>
2024-09-17 04:59:59 +0000

    commit 17-sep-24 10:31 AM

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,12 +12,25 @@ function Sidebar() {
   
   return (
     <div
-      className={`h-screen fixed top-0 right-0 bg-opacity-20 bg-slate-500 w-[10vw] min-w-[30px] max-w-[60px] flex text-center justify-center items-center z-2 z-${zIndex.sideBar}`}
+      className={`h-screen fixed top-0 right-0 w-[10vw] min-w-[35px] max-w-[60px] mr-2 md:-mr-2 flex text-center justify-center items-center sidebar-container z-2 z-${zIndex.sideBar}`}
     >
+      <div className="menu-bar flex flex-col">
+        {navRoutes.map((nav) => {
+        
+        return (<a key={nav.id} aria-label={nav.title} title={nav.title}
+          href={nav.path}
+          onClick={() => navigate(nav.path)}
+          className="p-2 my-1 header-nav-item cursor-pointer"
+        >
+          <span className="menu-span-item"><span className="text-xl lg:text-3xl"><IconComp componentName={nav.icon} /></span></span>
+          
+        </a>)
+      })}
+      </div>
       {/* <div className="w-full h-full absolute bg-white"></div> */}
       <div className="flex flex-col menu-wrapper">
 
-      {navRoutes.map((nav) => {
+      {/* {navRoutes.map((nav) => {
         
         return (<a key={nav.id}
           href={nav.path}
@@ -25,9 +38,9 @@ function Sidebar() {
           className="p-2 my-1 header-nav-item cursor-pointer"
         >
           <span className="menu-span-item"><span className="text-xl lg:text-3xl"><IconComp componentName={nav.icon} /></span></span>
-          {/* <IoHome /> */}
+          
         </a>)
-      })}
+      })} */}
       {/* <a href="/#" onClick={() => navigate('/#')}
         className="p-2 header-nav-item cursor-pointer"
       > <IoHome /> </a> */}

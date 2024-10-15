6aba6738c2aec479d2d601d9701b4ecb56f879b4
tanmayKumarSarkar <tanmayKumarSarkar@users.noreply.github.com>
2024-10-15 17:48:36 +0000

    deploy: 06cede3c968b1ef522f9589fcec376897dc02e9a

--- assets/MovingRectBG-ChCT3pao.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CVVjpV91.js";function i(){return s.jsx("div",{className:"area w-full h-full",children:s.jsxs("ul",{className:"squares",children:[s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{})]})})}export{i as default};

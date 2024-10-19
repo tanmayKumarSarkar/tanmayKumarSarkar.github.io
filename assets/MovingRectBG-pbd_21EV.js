bbb5ffca2a909548ba56e52af23ae5bbb9214446
tanmayKumarSarkar <tanmayKumarSarkar@users.noreply.github.com>
2024-10-19 16:26:58 +0000

    deploy: fda7b345d37d4f1cada3bfe3f963a2ec3493c6c2

---
+++ assets/MovingRectBG-pbd_21EV.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BEFx5QiZ.js";function i(){return s.jsx("div",{className:"area w-full h-full",children:s.jsxs("ul",{className:"squares",children:[s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{}),s.jsx("li",{})]})})}export{i as default};

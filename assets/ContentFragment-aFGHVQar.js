5362db2ea65b8fc20a8796015651381747828912
tanmayKumarSarkar <tanmayKumarSarkar@users.noreply.github.com>
2024-11-23 18:28:09 +0000

    deploy: a391870c2bc6aa28f6b60bf1fdb3c4b14f0be19a

---
+++ assets/ContentFragment-aFGHVQar.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-T3027joM.js";function a({id:t,className:e,children:n}){return r.jsx("div",{id:t,className:`${e}`,children:n})}export{a as default};
---
+++ assets/Skill-Yy2CJUwb.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-T3027joM.js";const r=()=>s.jsx("div",{className:"",children:"skills"});export{r as default};
